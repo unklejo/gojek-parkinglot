e73dcd0e070b05a2c8af872caeaf9c319cbbd7e2
jhovie.prastianto <jhovie.prastianto@DMSM-00006.jatis.com>
2017-11-19 21:50:09 +0700

    add basic test case for create parking lot and register car

---
+++ src/test/java/test/parkinglot/RegisterCarTest.java
@@ -0,0 +1,67 @@
+package test.parkinglot;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class RegisterCarTest {
+
+	@Test
+	public void testCreateParkingLotMoreThanOne() {
+		ParkingLotService service = new ParkingLotService();
+		Assert.assertEquals("Created a parking lot with 6 slots",
+				service.test("create_parking_lot 6"));
+	}
+
+	@Test
+	public void testCreateParkingLotOnlyOne() {
+		ParkingLotService service = new ParkingLotService();
+		Assert.assertEquals("Created a parking lot with 1 slot",
+				service.test("create_parking_lot 1"));
+	}
+	
+	@Test
+	public void testCreateParkingLotNoParams() {
+		ParkingLotService service = new ParkingLotService();
+		Assert.assertEquals("Variable input must be a number",
+				service.test("create_parking_lot"));
+	}
+
+	@Test
+	public void testCreateParkingLotNotNumber1() {
+		ParkingLotService service = new ParkingLotService();
+		Assert.assertEquals("Variable input must be a number",
+				service.test("create_parking_lot aaa"));
+	}
+
+	@Test
+	public void testCreateParkingLotNotNumber2() {
+		ParkingLotService service = new ParkingLotService();
+		Assert.assertEquals("Variable input must be a number",
+				service.test("create_parking_lot  "));
+	}
+	
+	@Test
+	public void testCreateParkingLotMoreThanOneParameters() {
+		ParkingLotService service = new ParkingLotService();
+		Assert.assertEquals("Variable input must be a number",
+				service.test("create_parking_lot 3 3"));
+	}
+
+	@Test
+	public void testCreateParkingLotAlreadyCreatedOnlyOneSlot() {
+		ParkingLotService service = new ParkingLotService();
+		service.test("create_parking_lot 1");
+		Assert.assertEquals("Parking lot is already created with 1 slot",
+				service.test("create_parking_lot 3"));
+	}
+	
+	@Test
+	public void testCreateParkingLotAlreadyCreatedMoreThanOneSlots() {
+		ParkingLotService service = new ParkingLotService();
+		service.test("create_parking_lot 2");
+		Assert.assertEquals("Parking lot is already created with 2 slots",
+				service.test("create_parking_lot 3"));
+	}
+
+}
